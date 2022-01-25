d2970efdb5c0e9c1b4c7c31e5c48502a7202e71e
Matheus Soares <matheus.asoares2@gmail.com>
2022-01-25 01:57:54 -0300

    reformat

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ function convertTableToCSV(tableContent) {
     var operations = div.getElementsByClassName("table-content__item pointer")
     var result = "Data;Papel;Qtde;Valor;Tipo\n"
     var skippedOperations = 0
+
     for (let op of operations) {
         // Check if the value is positive
         if (op.getElementsByClassName("value positive icon-positive").length > 0) {
@@ -12,21 +13,21 @@ function convertTableToCSV(tableContent) {
             var description = op.getElementsByClassName("description")[0].getElementsByClassName("description soma-caption hydrated")[0].innerText.replace(/\s+/g, "").trim()
 
             if (description.includes("RENDIMENTO")) {
-                const regexRendimentos =  new RegExp("RENDIMENTO(.+)PAPEL(.+)", "g")
+                const regexRendimentos = new RegExp("RENDIMENTO(.+)PAPEL(.+)", "g")
                 const [_, quantity, ticket] = regexRendimentos.exec(description)
                 result += [settlement, ticket, quantity, value, "Rendimento", "\n"].join(";")
             } else if (description.includes("JUROSS/CAPITAL")) {
                 const regexJuros = new RegExp("JUROSS\/CAPITAL([0-9]+)([A-Z0-9]+)", "g")
                 const [_, quantity, ticket] = regexJuros.exec(description)
-                result += [settlement, ticket, quantity, value,  "Juros s/ Capital", "\n"].join(";")
+                result += [settlement, ticket, quantity, value, "Juros s/ Capital", "\n"].join(";")
             } else if (description.includes("DIVIDENDOS")) {
                 const regexDividendos = new RegExp("DIVIDENDOS([0-9]+)([A-Z0-9]+)")
                 const [_, quantity, ticket] = regexDividendos.exec(description)
-                result += [settlement, ticket, quantity, value,  "Dividendos", "\n"].join(";")
+                result += [settlement, ticket, quantity, value, "Dividendos", "\n"].join(";")
             } else skippedOperations++
         }
     }
-    result += "\n\n Skipped Operations = " + skippedOperations
+    result += "\n\nSkipped Operations = " + skippedOperations
 
     document.getElementById("resultText").value = result
 }
